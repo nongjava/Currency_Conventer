a512167c1257989ec1fe28402cd48c4065c84bd6
nongjava <64765621+nongjava@users.noreply.github.com>
2020-05-14 09:44:49 +0700

    Update sever.js

--- sever.js
+++ sever.js
@@ -3,7 +3,7 @@ var fs = require('fs');
  
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./os_1.html', null, function (error, data) {
+    fs.readFile('./Os.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
             respone.write('Whoops! File not found!');
